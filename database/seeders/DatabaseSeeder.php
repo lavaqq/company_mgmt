8f67ff3530b66e177d708321d78790e9ea8ef8c6
lavaqq <mlava@tuta.io>
2023-04-06 14:27:16 +0200

    feat(seeder): dev user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $dev_user = new User();
+        $dev_user->name = 'dev';
+        $dev_user->email = 'dev@dev.io';
+        $dev_user->password = Hash::make('dev');
+        $dev_user->save();
     }
 }
