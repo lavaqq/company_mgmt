2978664ab9a8759d0ef516d2976d8cd0adc64d91
lavaqq <mlava@tuta.io>
2023-04-07 13:44:59 +0200

    feat(db): reccuring invoices migration

---
+++ database/migrations/2023_04_07_104705_create_reccuring_invoices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reccuring_invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained()->cascadeOnDelete();
+            $table->enum('frequency', ['weekly', 'monthly', 'quarterly', 'yearly']);
+            $table->date('start_date');
+            $table->boolean('is_indefinite_duration')->default(false);
+            $table->date('end_date');
+            $table->decimal('tax_rate', 10, 2);
+            $table->decimal('total_excl_tax', 10, 2);
+            $table->decimal('total_incl_tax', 10, 2);
+            $table->decimal('total_tax', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reccuring_invoices');
+    }
+};
