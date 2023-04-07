20cfc153219d8a4ff8bf443ac2a3c980e3514046
lavaqq <mlava@tuta.io>
2023-04-07 13:48:18 +0200

    feat(provider): register all observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\Adress;
+use App\Models\Company;
+use App\Models\Invoice;
+use App\Models\InvoiceDiscount;
+use App\Models\InvoiceItem;
+use App\Models\Person;
+use App\Models\ReccuringInvoice;
+use App\Models\ReccuringInvoiceItem;
+use App\Models\Transaction;
+use App\Observers\PersonObserver;
 use Illuminate\Support\ServiceProvider;
 use Filament\Facades\Filament;
 
@@ -23,5 +33,13 @@ class AppServiceProvider extends ServiceProvider
         Filament::serving(function () {
             Filament::registerViteTheme('resources/css/filament.css');
         });
+        Adress::observe(AdressObserver::class);
+        Company::observe(CompanyObserver::class);
+        InvoiceDiscount::observe(InvoiceDiscountObserver::class);
+        InvoiceItem::observe(InvoiceItemObserver::class);
+        Invoice::observe(InvoiceObserver::class);
+        ReccuringInvoiceItem::observe(ReccuringInvoiceItemObserver::class);
+        ReccuringInvoice::observe(ReccuringInvoiceObserver::class);
+        Transaction::observe(TransactionObserver::class);
     }
 }
