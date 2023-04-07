87b1db97b0ddd3eb4c4d1de8efac279055f16b4f
lavaqq <mlava@tuta.io>
2023-04-07 13:46:49 +0200

    feat(model): reccuring invoice relations and use soft deletes

---
+++ app/Models/ReccuringInvoice.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ReccuringInvoice extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $guarded = [];
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+    public function reccuringInvoiceItems(): HasMany
+    {
+        return $this->hasMany(ReccuringInvoiceItem::class);
+    }
+}
