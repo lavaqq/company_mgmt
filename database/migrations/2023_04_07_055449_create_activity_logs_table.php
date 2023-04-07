f559f25f12329efe893caf3c295bac630abea300
lavaqq <mlava@tuta.io>
2023-04-07 12:39:30 +0200

    feat(db): actvity log table and model

---
+++ database/migrations/2023_04_07_055449_create_activity_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('user_id');
+            $table->enum('action', ['create', 'update', 'restore', 'delete', 'force_delete']);
+            $table->string('table');
+            $table->integer('record_id');
+            $table->json('data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activity_logs');
+    }
+};
