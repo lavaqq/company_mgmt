2e8f448149ef341286519abd6751949e284a3c55
lavaqq <mlava@tuta.io>
2023-04-11 12:25:16 +0200

    feat(model): invoice relation and process calculating

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -24,8 +24,55 @@ class Invoice extends Model
     {
         return $this->hasMany(InvoiceDiscount::class);
     }
-    public function transactions(): HasMany
+    public static function genVCS()
     {
-        return $this->hasMany(Transaction::class);
+        $count = self::whereNull('deleted_at')->count();
+        $vcsStart = str_pad($count + 1, 10, "0", STR_PAD_LEFT);
+        $verificationNumber = round(($count + 1 / 97 - floor($count + 1 / 97)) * 97);
+        if ($verificationNumber === 0) {
+            $verificationNumber = 97;
+        }
+        $tempVCS = $vcsStart . str_pad($verificationNumber, 2, "0", STR_PAD_LEFT);
+        $vcs = "+++" . substr($tempVCS, 0, 3) . "/" . substr($tempVCS, 3, 4) . "/" . substr($tempVCS, 7, 5) . "+++";
+        return $vcs;
+    }
+    protected static function genInvoiceNum()
+    {
+        $count = self::whereNull('deleted_at')->count();
+        $invoice_number = 'LS-' . str_pad((string) ($count + 1), 4, '0', STR_PAD_LEFT);
+        return $invoice_number;
+    }
+    public function getTotalExclTax(): float
+    {
+        $totalItem = $this->invoiceItems()->sum('amount');
+        $totalDiscountEuro = $this->invoiceDiscounts()->where('is_percentage', '!=', true)->sum('amount');
+        $totalDiscountPerc = $this->invoiceDiscounts()->where('is_percentage', '=', true)->sum('amount');
+        $totalItem -= $totalDiscountEuro;
+        $totalItem -= $totalItem * ($totalDiscountPerc / 100);
+        $totalExclTax = $totalItem;
+        return $totalExclTax;
+    }
+    public function getTotalInclTax(): float
+    {
+        $totalItem = $this->invoiceItems()->sum('amount');
+        $totalDiscountEuro = $this->invoiceDiscounts()->where('is_percentage', '!=', true)->sum('amount');
+        $totalDiscountPerc = $this->invoiceDiscounts()->where('is_percentage', '=', true)->sum('amount');
+        $taxRate = $this->tax_rate;
+        $totalItem -= $totalDiscountEuro;
+        $totalItem -= $totalItem * ($totalDiscountPerc / 100);
+        $totalItem += $totalItem * ($taxRate / 100);
+        $totalInclTax = $totalItem;
+        return $totalInclTax;
+    }
+    public function getTotalTax(): float
+    {
+        $totalItem = $this->invoiceItems()->sum('amount');
+        $totalDiscountEuro = $this->invoiceDiscounts()->where('is_percentage', '!=', true)->sum('amount');
+        $totalDiscountPerc = $this->invoiceDiscounts()->where('is_percentage', '=', true)->sum('amount');
+        $taxRate = $this->tax_rate;
+        $totalItem -= $totalDiscountEuro;
+        $totalItem -= $totalItem * ($totalDiscountPerc / 100);
+        $totalTax = $totalItem * ($taxRate / 100);
+        return $totalTax;
     }
 }
