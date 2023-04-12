8d81ddeee6a594560e0d11e48c2a99472d73b611
lavaqq <mlava@tuta.io>
2023-04-12 22:29:16 +0200

    test: temp routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\GenInvoiceController;
+use App\Http\Controllers\RandomController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+
+Route::get('/view', [GenInvoiceController::class, 'view']);
+Route::get('/test', [GenInvoiceController::class, 'test']);
