5cd357f3a25c4bb4a5f2a8be3e7128690b5c9508
lavaqq <mlava@tuta.io>
2023-04-13 12:53:44 +0200

    feat(invoice): fn gen qr code

--- app/Http/Controllers/GenInvoiceController.php
+++ app/Http/Controllers/GenInvoiceController.php
@@ -4,35 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Invoice;
 use Dompdf\Dompdf;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class GenInvoiceController extends Controller
 {
 
     public static function test()
     {
-        $invoice = Invoice::all()->first();
-        $data = [
-            'invoice_number' => $invoice->invoice_number,
-            'vcs' => $invoice->vcs,
-            'issue_date' => $invoice->issue_date,
-            'due_date' => $invoice->due_date,
-            'tax_rate' => $invoice->tax_rate,
-            'invoice_items' => $invoice->invoiceItems,
-            'invoice_discounts' => $invoice->invoiceDiscounts,
-            'get_total_excl_tax' => $invoice->getTotalExclTax(),
-            'get_total_incl_tax' => $invoice->getTotalInclTax(),
-            'get_total_tax' => $invoice->getTotalTax(),
-            'name' => $invoice->company->name,
-            'legal_form' => $invoice->company->legal_form,
-            'vat_number' => $invoice->company->vat_number,
-            'street' => $invoice->company->street,
-            'number' => $invoice->company->number,
-            'box' => $invoice->company->box,
-            'city' => $invoice->company->city,
-            'zipcode' => $invoice->company->zipcode,
-            'country' => $invoice->company->country,
-        ];
-        return view('pdf.invoice', $data)->render();
+        return self::genQRCode(Invoice::all()->first());
+    }
+
+    public static function genQRCode(Invoice $invoice) : string {
+        $defaultParameters = 'BCD 001 1 SCT XXXXXX LOAK.STUDIO BEXXXXXXXXXXXX';
+        $total = $invoice->getTotalInclTax();
+        $vcs = $invoice->vcs;
+        return QrCode::generate($defaultParameters . ' EUR' . $total . ' ' . $vcs);
     }
 
     public static function view($id)
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('invoices/{id}/pdf', [GenInvoiceController::class, 'view'])
     ->middleware(Authenticate::class)
-    ->name('invoice.pdf');;
+    ->name('invoice.pdf');
+
+    Route::get('test', [GenInvoiceController::class, 'test']);
