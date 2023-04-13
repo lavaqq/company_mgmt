296ef68076cb3e74e35b8045446364f843089617
lavaqq <mlava@tuta.io>
2023-04-13 10:26:13 +0200

    feat(pdf.view): add reset css

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -1,11 +1,143 @@
+<!DOCTYPE html>
 <html>
 
 <head>
     <title>{{ $invoice_number }}</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <style>
+        html,
+        body,
+        div,
+        span,
+        applet,
+        object,
+        iframe,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        blockquote,
+        pre,
+        a,
+        abbr,
+        acronym,
+        address,
+        big,
+        cite,
+        code,
+        del,
+        dfn,
+        em,
+        img,
+        ins,
+        kbd,
+        q,
+        s,
+        samp,
+        small,
+        strike,
+        strong,
+        sub,
+        sup,
+        tt,
+        var,
+        b,
+        u,
+        i,
+        center,
+        dl,
+        dt,
+        dd,
+        ol,
+        ul,
+        li,
+        fieldset,
+        form,
+        label,
+        legend,
+        table,
+        caption,
+        tbody,
+        tfoot,
+        thead,
+        tr,
+        th,
+        td,
+        article,
+        aside,
+        canvas,
+        details,
+        embed,
+        figure,
+        figcaption,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        output,
+        ruby,
+        section,
+        summary,
+        time,
+        mark,
+        audio,
+        video {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font-size: 100%;
+            font: inherit;
+            vertical-align: baseline;
+        }
+
+        article,
+        aside,
+        details,
+        figcaption,
+        figure,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        section {
+            display: block;
+        }
+
         body {
-            font-family: 'Poppins', sans-serif;
+            line-height: 1;
+        }
+
+        ol,
+        ul {
+            list-style: none;
+        }
+
+        blockquote,
+        q {
+            quotes: none;
+        }
+
+        blockquote:before,
+        blockquote:after,
+        q:before,
+        q:after {
+            content: '';
+            content: none;
+        }
+
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        body {
+            font-family: 'Poppins';
+            margin: 60px;
         }
 
         .header__logo {
@@ -135,7 +267,6 @@
         }
 
         .payement {
-            width: 350px;
             font-weight: 500;
             font-size: 8px;
             line-height: 164.5%;
@@ -147,13 +278,13 @@
     </style>
 </head>
 
-<body>
+<body class="poppins">
     <div class="invoice__header">
         <img class="header__logo"
             src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('/logo.png'))) }}">
         <div class="header__info">
             <div class="header__address">
-                <p>LOAK.STUDIO (SRL)</p>
+                <p class="poppins">LOAK.STUDIO (SRL)</p>
                 <p>RUE DU MOULIN A EAU (WI) 30</p>
                 <p>7506 TOURNAI BELGIQUE</p>
                 <p>BE0795704361</p>
@@ -229,7 +360,7 @@
         </div>
     </div>
     <p class="payement">
-        Veuillez payer le montant dû avant la date d'échéance sur le compte BE74 0689 4674 7107 avec
+        Veuillez payer le montant dû avant la date d'échéance sur le compte BE74 0689 4674 7107 <br> avec
         {{ $vcs }}
         en tant que communication structurée.
     </p>
