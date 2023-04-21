cc2ec55a9885e8bfdd2121935e283f713447fcc4
lavaqq <mlava@tuta.io>
2023-04-21 16:22:16 +0200

    fix(db): nullable vcs

--- database/migrations/2023_04_07_104555_create_invoices_table.php
+++ database/migrations/2023_04_07_104555_create_invoices_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('company_id')->constrained()->cascadeOnDelete();
             $table->string('invoice_number');
-            $table->string('vcs');
+            $table->string('vcs')->nullable();
             $table->date('issue_date');
             $table->date('due_date');
             $table->decimal('tax_rate', 10, 2);
