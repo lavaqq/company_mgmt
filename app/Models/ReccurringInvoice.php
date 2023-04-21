71f22cf991d9c496015cf3cf9232fa349418e1fc
lavaqq <mlava@tuta.io>
2023-04-21 22:32:38 +0200

    feat(reccurring): main fn

--- app/Models/ReccurringInvoice.php
+++ app/Models/ReccurringInvoice.php
@@ -47,4 +47,92 @@ class ReccurringInvoice extends Model
         $totalTax = $totalItem * ($taxRate / 100);
         return $totalTax;
     }
+
+    public static function scopeActive()
+    {
+        $currentDate = Carbon::now();
+        $reccurringInvoices = ReccurringInvoice::where(function ($query) use ($currentDate) {
+            $query->where('end_date', '>=', $currentDate)
+                ->orWhere('is_indefinite_duration', true);
+        })
+            ->get();
+        return $reccurringInvoices;
+    }
+
+    public function getIssueDates()
+    {
+        $startDate = Carbon::parse($this->start_date);
+        $frequency = $this->frequency;
+        $issueDates = array();
+
+        if ($this->is_indefinite_duration) {
+            $endDate = $startDate->copy()->addYear();
+        } else {
+            $endDate = Carbon::parse($this->end_date);
+        }
+
+        switch ($frequency) {
+            case 'weekly':
+                $interval = '1 week';
+                break;
+            case 'monthly':
+                $interval = '1 month';
+                break;
+            case 'quarterly':
+                $interval = '3 months';
+                break;
+            case 'yearly':
+                $interval = '1 year';
+                break;
+        }
+        $date = $startDate->copy();
+        while ($date <= $endDate) {
+            $issueDates[] = $date->format('Y-m-d');
+            $date->add($interval);
+        }
+        return $issueDates;
+    }
+
+    public function needsToCreateInvoice()
+    {
+        $currentDate = Carbon::now();
+        $startDate = Carbon::parse($this->start_date);
+        $frequency = $this->frequency;
+        $latestInvoice = $this->invoices()->latest()->first();
+        if (!$latestInvoice) {
+            return true;
+        }
+        if ($latestInvoice->issue_date < $startDate->format('Y-m-d')) {
+            return true;
+        }
+        $issueDates = $this->getIssueDates();
+        $nextIssueDate = null;
+        foreach ($issueDates as $issueDate) {
+            if ($issueDate > $latestInvoice->issue_date) {
+                $nextIssueDate = $issueDate;
+                break;
+            }
+        }
+        if ($nextIssueDate && $nextIssueDate <= $currentDate->format('Y-m-d')) {
+            return false;
+        }
+        switch ($frequency) {
+            case 'weekly':
+                $interval = '1 week';
+                break;
+            case 'monthly':
+                $interval = '1 month';
+                break;
+            case 'quarterly':
+                $interval = '3 months';
+                break;
+            case 'yearly':
+                $interval = '1 year';
+                break;
+        }
+        if ($this->is_indefinite_duration && $latestInvoice->issue_date >= $currentDate->copy()->sub($interval)->format('Y-m-d')) {
+            return true;
+        }
+        return false;
+    }
 }
