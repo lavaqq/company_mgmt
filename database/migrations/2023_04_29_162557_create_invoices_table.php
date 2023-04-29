c61f67c242130d51bec86a483c06fad805c175ae
lavaqq <mlava@tuta.io>
2023-04-29 19:06:03 +0200

    feat(db): invoice migration

---
+++ database/migrations/2023_04_29_162557_create_invoices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id');
+            $table->string('reference');
+            $table->string('vcs')->nullable();
+            $table->date('issue_date');
+            $table->date('due_date');
+            $table->decimal('tax_rate', 10, 2);
+            $table->decimal('total_excl_tax', 10, 2);
+            $table->decimal('total_incl_tax', 10, 2);
+            $table->enum('status', ['draft', 'pending', 'paid']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
