a720af8030c266fbed147e163aca43ecc68aac7f
lavaqq <mlava@tuta.io>
2023-04-30 16:55:57 +0200

    feat(filament): identity sheet

---
+++ app/Filament/Pages/IdentitySheet.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class IdentitySheet extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static ?string $navigationLabel = "Fiche d'identité";
+
+    protected function getTitle(): string
+    {
+        return "Fiche d'identité";
+    }
+
+    protected static string $view = 'filament.pages.identity-sheet';
+}
