5154286e0514093149e829a31daf829a90b7b00f
lavaqq <mlava@tuta.io>
2023-04-30 15:43:10 +0200

    feat(model): get totals & vcs/reference generation

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -37,4 +37,53 @@ class Invoice extends Model
     {
         return $this->hasMany(InvoiceDiscount::class);
     }
+
+    public static function generateVCS(): string
+    {
+        $count = self::count();
+        $vcsStart = str_pad($count + 1, 10, "0", STR_PAD_LEFT);
+        $verificationNumber = (98 - ($count + 1) % 97) % 97;
+        $tempVCS = $vcsStart . str_pad($verificationNumber, 2, "0", STR_PAD_LEFT);
+        return "+++" . substr_replace(substr_replace(substr($tempVCS, 0, 3), "/", 1, 0), "/", 5, 0) . "+++";
+    }
+
+    protected static function generateReference(): string
+    {
+        $count = self::count();
+        return "LS-" . str_pad($count + 1, 4, "0", STR_PAD_LEFT);
+    }
+
+    public function getTotalExcludingTax(): float
+    {
+        $totalItem = $this->items()->sum('amount');
+        $totalDiscountEuro = $this->discounts()->where('is_percentage', false)->sum('amount');
+        $totalDiscountPerc = $this->discounts()->where('is_percentage', true)->sum('amount');
+        $totalItem -= $totalDiscountEuro;
+        $totalItem *= (1 - $totalDiscountPerc / 100);
+        return $totalItem;
+    }
+
+    public function getTotalIncludingTax(): float
+    {
+        $totalItem = $this->items()->sum('amount');
+        $totalFixedDiscount = $this->discounts()->where('is_percentage', false)->sum('amount');
+        $totalPercentageDiscount = $this->discounts()->where('is_percentage', true)->sum('amount');
+        $taxRate = $this->tax_rate;
+        $totalItem -= $totalFixedDiscount;
+        $totalItem *= (1 - $totalPercentageDiscount / 100);
+        $totalItem *= (1 + $taxRate / 100);
+        return $totalItem;
+    }
+
+    public function getTax(): float
+    {
+        $totalItem = $this->items()->sum('amount');
+        $totalFixedDiscount = $this->discounts()->where('is_percentage', false)->sum('amount');
+        $totalPercentageDiscount = $this->discounts()->where('is_percentage', true)->sum('amount');
+        $taxRate = $this->tax_rate;
+        $totalItem -= $totalFixedDiscount;
+        $totalItem *= (1 - $totalPercentageDiscount / 100);
+        $totalTax = $totalItem * $taxRate / 100;
+        return $totalTax;
+    }
 }
