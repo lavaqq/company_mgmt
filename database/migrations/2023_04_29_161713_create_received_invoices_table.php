479c8e915bcee9ead0265bbc3cb843f43c62be2b
lavaqq <mlava@tuta.io>
2023-04-30 15:48:57 +0200

    chore(db): useless fields

--- database/migrations/2023_04_29_161713_create_received_invoices_table.php
+++ database/migrations/2023_04_29_161713_create_received_invoices_table.php
@@ -14,13 +14,10 @@ return new class extends Migration
         Schema::create('received_invoices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_id');
-            $table->string('reference')->nullable();
             $table->date('issue_date');
-            $table->date('due_date');
             $table->decimal('tax_rate', 10, 2);
             $table->decimal('total_excl_tax', 10, 2);
             $table->decimal('total_incl_tax', 10, 2);
-            $table->enum('status', ['paid', 'unpaid']);
             $table->timestamps();
         });
     }
