f3e046f514e2e9e4ac611b91723998a281292328
lavaqq <mlava@tuta.io>
2023-04-30 23:57:07 +0200

    feat(filament): account link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Filament\Facades\Filament;
+use Filament\Navigation\UserMenuItem;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Filament::serving(function () {
             Filament::registerViteTheme('resources/css/filament.css');
+            Filament::registerUserMenuItems([
+                'account' => UserMenuItem::make()->url(route('filament.resources.users.edit', ['record' => auth()->user()->id]))
+            ]);
         });
     }
 }
