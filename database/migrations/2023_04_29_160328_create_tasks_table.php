7544f6105e0828ba9eb40361c021e2ab1f6ab7cf
lavaqq <mlava@tuta.io>
2023-04-30 18:36:56 +0200

    feat(db): add status

--- database/migrations/2023_04_29_160328_create_tasks_table.php
+++ database/migrations/2023_04_29_160328_create_tasks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longText('description');
+            $table->enum('status', ['pending', 'in_progress', 'done']);
             $table->timestamps();
         });
     }
