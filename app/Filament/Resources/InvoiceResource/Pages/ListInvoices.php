d69517d3f9e91a5a9b39613f1e29c9d3bd5e7877
lavaqq <mlava@tuta.io>
2023-04-30 15:27:56 +0200

    feat(filament): invoice pages

---
+++ app/Filament/Resources/InvoiceResource/Pages/ListInvoices.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\InvoiceResource\Pages;
+
+use App\Filament\Resources\InvoiceResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListInvoices extends ListRecords
+{
+    protected static string $resource = InvoiceResource::class;
+    protected function getActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('Créer une facture'),
+        ];
+    }
+    protected function getTitle(): string
+    {
+        return 'Gestion des factures émises';
+    }
+}
