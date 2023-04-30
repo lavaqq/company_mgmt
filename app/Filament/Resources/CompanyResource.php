fd102742d71dba8977465ebb704c19b3a7642b20
lavaqq <mlava@tuta.io>
2023-04-30 13:51:43 +0200

    feat(filament): company ressource

---
+++ app/Filament/Resources/CompanyResource.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CompanyResource\Pages;
+use App\Filament\Resources\CompanyResource\RelationManagers;
+use App\Models\Company;
+use Carbon\Carbon;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CompanyResource extends Resource
+{
+    protected static ?string $model = Company::class;
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $label = 'Entreprise';
+    protected static ?string $pluralLabel = 'Entreprises';
+    protected static ?string $navigationLabel = 'Entreprises';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nom')
+                            ->required(),
+                        TextInput::make('vat_number')
+                            ->label('Numéro de TVA')
+                            ->required(),
+                        Select::make('legal_form')
+                            ->label('Forme légal')
+                            ->options([
+                                "SA" => "SA (Société Anonyme)",
+                                "SAS" => "SAS (Société par Actions Simplifiée)",
+                                "SNC" => "SNC (Société en Nom Collectif)",
+                                "SCS" => "SCS (Société en Commandite Simple)",
+                                "SCOP" => "SCOP (Société Coopérative et Participative)",
+                                "SCM" => "SCM (Société Civile de Moyens)",
+                                "SELARL" => "SELARL (Société d'Exercice Libéral à Responsabilité Limitée)",
+                                "SCI" => "SCI (Société Civile Immobilière)",
+                                "EURL" => "EURL (Entreprise Unipersonnelle à Responsabilité Limitée)",
+                                "SASU" => "SASU (Société par Actions Simplifiée Unipersonnelle)",
+                                "SEP" => "SEP (Société en Participation)",
+                                "SELAS" => "SELAS (Société d'Exercice Libéral par Actions Simplifiée)",
+                                "SELAFA" => "SELAFA (Société d'Exercice Libéral à Forme Anonyme)",
+                                "SEM" => "SEM (Société d'Economie Mixte)",
+                                "SCA" => "SCA (Société en Commandite par Actions)",
+                                "SRL" => "SRL (Société à Responsabilité Limitée)",
+                                "SARL" => "SARL (Société à Responsabilité Limitée)",
+                                "SPRL" => "SPRL (Société Privée à Responsabilité Limitée)",
+                            ])
+                            ->searchable()
+                            ->required(),
+                    ])->columns(3),
+                Card::make()
+                    ->relationship('adress')
+                    ->schema([
+                        TextInput::make('street')
+                            ->label('Rue')
+                            ->required(),
+                        TextInput::make('zipcode')
+                            ->label('Code postal')
+                            ->numeric()
+                            ->required(),
+                        TextInput::make('number')
+                            ->label('Numéro')
+                            ->numeric()
+                            ->required(),
+                        TextInput::make('city')
+                            ->label('Ville')
+                            ->required(),
+                        TextInput::make('box')
+                            ->label('Boîte'),
+                        Select::make('country')
+                            ->label('Pays')
+                            ->options([
+                                'Allemagne' => 'Allemagne',
+                                'Autriche' => 'Autriche',
+                                'Belgique' => 'Belgique',
+                                'Bulgarie' => 'Bulgarie',
+                                'Chypre' => 'Chypre',
+                                'Croatie' => 'Croatie',
+                                'Danemark' => 'Danemark',
+                                'Espagne' => 'Espagne',
+                                'Estonie' => 'Estonie',
+                                'Finlande' => 'Finlande',
+                                'France' => 'France',
+                                'Grèce' => 'Grèce',
+                                'Hongrie' => 'Hongrie',
+                                'Irlande' => 'Irlande',
+                                'Italie' => 'Italie',
+                                'Lettonie' => 'Lettonie',
+                                'Lituanie' => 'Lituanie',
+                                'Luxembourg' => 'Luxembourg',
+                                'Malte' => 'Malte',
+                                'Pays-Bas' => 'Pays-Bas',
+                                'Pologne' => 'Pologne',
+                                'Portugal' => 'Portugal',
+                                'Roumanie' => 'Roumanie',
+                                'Slovaquie' => 'Slovaquie',
+                                'Slovénie' => 'Slovénie',
+                                'Suède' => 'Suède',
+                                'Tchéquie' => 'Tchéquie',
+                            ])
+                            ->searchable()
+                            ->required(),
+                    ])->columns(2)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('legal_form'),
+                TextColumn::make('vat_number'),
+                TextColumn::make('updated_at')
+                    ->getStateUsing(function (Model $record): string {
+                        return Carbon::parse($record->updated_at)->diffForHumans();
+                    })
+                    ->label('Dernière modification'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make()
+                    ->modalHeading("Supprimer la sélection d'entreprises"),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompanies::route('/'),
+            'create' => Pages\CreateCompany::route('/create'),
+            'edit' => Pages\EditCompany::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/CompanyResource/Pages/CreateCompany.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\CompanyResource\Pages;
+
+use App\Filament\Resources\CompanyResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCompany extends CreateRecord
+{
+    protected static string $resource = CompanyResource::class;
+    protected function getTitle(): string
+    {
+        return 'Créer une entreprise';
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+    protected static bool $canCreateAnother = false;
+
+}
