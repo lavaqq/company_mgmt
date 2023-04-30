3f333cd3fb0cbf2c7deb115ce40bb7ee7718d995
lavaqq <mlava@tuta.io>
2023-04-30 11:13:03 +0200

    feat(model): task relationships

--- app/Models/SubTask.php
+++ app/Models/SubTask.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SubTask extends Model
 {
@@ -14,4 +15,9 @@ class SubTask extends Model
         'title',
         'description',
     ];
+
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
 }
