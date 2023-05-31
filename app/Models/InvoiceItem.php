934846ea9f33f6a783f4e3189c2671e76747c1f7
lavaqq <mlava@tuta.io>
2023-05-31 18:19:13 +0200

    feat(db): invoice items table

---
+++ app/Models/InvoiceItem.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class InvoiceItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'invoice_id',
+        'description',
+        'amount',
+        'tax_rate'
+    ];
+}
