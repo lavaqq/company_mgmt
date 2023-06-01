7f6056e6a4f3236b4d9bb4af32996c0b46b72533
lavaqq <mlava@tuta.io>
2023-06-01 09:34:22 +0200

    feat(db): estimates table

---
+++ database/migrations/2023_06_01_071612_create_estimates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estimates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id');
+            $table->string('reference');
+            $table->decimal('tax_rate', 10, 2);
+            $table->date('issue_date');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estimates');
+    }
+};
