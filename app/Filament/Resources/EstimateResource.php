a19fc20e700a92ba203e3588a6293190750795ca
lavaqq <mlava@tuta.io>
2023-06-01 09:52:21 +0200

    feat(filament): list & form estimates

--- app/Filament/Resources/EstimateResource.php
+++ app/Filament/Resources/EstimateResource.php
@@ -5,12 +5,24 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\EstimateResource\Pages;
 use App\Filament\Resources\EstimateResource\RelationManagers;
 use App\Models\Estimate;
+use Carbon\Carbon;
+use Closure;
 use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class EstimateResource extends Resource
@@ -23,7 +35,81 @@ class EstimateResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Card::make()
+                    ->schema([
+                        Select::make('company_id')
+                            ->label('Entreprise')
+                            ->relationship('company', 'name')
+                            ->preload()
+                            ->placeholder('Sélectionnez une entreprise')
+                            ->required(),
+                        DatePicker::make('issue_date')
+                            ->label("Date d'émission")
+                            ->minDate(Carbon::now()->format('Y-m-d'))
+                            ->default(Carbon::now()->format('Y-m-d'))
+                            ->displayFormat('d/m/Y')
+                            ->required(),
+                        DatePicker::make('due_date')
+                            ->label("Date d'échéance")
+                            ->minDate(Carbon::now()->addDay(30)->format('Y-m-d'))
+                            ->default(Carbon::now()->addDay(30)->format('Y-m-d'))
+                            ->displayFormat('d/m/Y')
+                            ->required(),
+                        TextInput::make('reference')
+                            ->label('Numéro de facture')
+                            ->default(fn (): string => 'D-' . str_pad(Estimate::count() + 1, 4, '0', STR_PAD_LEFT))
+                            ->disabled()
+                            ->required(),
+                        TextInput::make('tax_rate')
+                            ->label('Taux TVA')
+                            ->numeric()
+                            ->default(21)
+                            ->suffix('%')
+                            ->required(),
+                    ])->columns(3),
+                Tabs::make('Facturation')
+                    ->tabs([
+                        Tab::make('Services')
+                            ->schema([
+                                Repeater::make('items')
+                                    ->defaultItems(0)
+                                    ->createItemButtonLabel('Ajouter un service')
+                                    ->relationship()
+                                    ->label('')
+                                    ->schema([
+                                        TextInput::make('description')
+                                            ->required()
+                                            ->columnSpan(2),
+                                        TextInput::make('amount')
+                                            ->label('Montant')
+                                            ->numeric()
+                                            ->suffix('€')
+                                            ->required(),
+                                    ])->columns(3),
+                            ]),
+                        Tab::make('Réductions')
+                            ->schema([
+                                Repeater::make('discounts')
+                                    ->defaultItems(0)
+                                    ->createItemButtonLabel('Ajouter une réduction')
+                                    ->relationship()
+                                    ->label('')
+                                    ->schema([
+                                        Toggle::make('is_percentage')
+                                            ->label('En pourcentage')
+                                            ->reactive()
+                                            ->columnSpan(3),
+                                        TextInput::make('description')
+                                            ->required()
+                                            ->columnSpan(2),
+                                        TextInput::make('amount')
+                                            ->label('Montant')
+                                            ->required()
+                                            ->numeric()
+                                            ->suffix(fn (Closure $get) => $get('is_percentage') ? '%' : '€'),
+                                    ])->columns(3),
+                            ]),
+                    ])->columnSpanFull(),
             ]);
     }
 
@@ -31,7 +117,30 @@ class EstimateResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('reference')
+                    ->label('Référence'),
+                TextColumn::make('company.name')
+                    ->label('Entreprise')
+                    ->limit(20),
+                TextColumn::make('issue_date')
+                    ->label("Date d'émission")
+                    ->dateTime('d/m/Y'),
+                TextColumn::make('due_date')
+                    ->label("Date d'échéance")
+                    ->dateTime('d/m/Y'),
+                TextColumn::make('total_excl_tax')
+                    ->label('Total HT')
+                    ->suffix(' €')
+                    ->getStateUsing(fn (Model $record): float => $record->getTotalExcludingTax()),
+                TextColumn::make('total_incl_tax')
+                    ->label('Total TTC')
+                    ->suffix(' €')
+                    ->getStateUsing(fn (Model $record): float => $record->getTotalIncludingTax()),
+                TextColumn::make('updated_at')
+                    ->getStateUsing(function (Model $record): string {
+                        return Carbon::parse($record->updated_at)->diffForHumans();
+                    })
+                    ->label('Dernière modification'),
             ])
             ->filters([
                 //
@@ -43,14 +152,14 @@ class EstimateResource extends Resource
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -58,5 +167,5 @@ class EstimateResource extends Resource
             'create' => Pages\CreateEstimate::route('/create'),
             'edit' => Pages\EditEstimate::route('/{record}/edit'),
         ];
-    }    
+    }
 }
