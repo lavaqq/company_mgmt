19cc7249598b91b3c72ac3c9045009205117b853
lavaqq <mlava@tuta.io>
2023-06-02 11:28:49 +0200

    feat(controller): pdf generation

--- app/Http/Controllers/EstimateController.php
+++ app/Http/Controllers/EstimateController.php
@@ -3,16 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\Models\Estimate;
+use Dompdf\Dompdf;
 
 class EstimateController extends Controller
 {
     public function stream(Estimate $record)
     {
-        return $record;
+        $html = view('estimate.pdf', ['data' => $record])->render();
+        $tmp = sys_get_temp_dir();
+        $dompdf = new Dompdf([
+            'logOutputFile' => '',
+            'isRemoteEnabled' => true,
+            'fontDir' => $tmp,
+            'fontCache' => $tmp,
+            'tempDir' => $tmp,
+            'chroot' => $tmp,
+        ]);
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+        $dompdf->stream($record->reference . ' (' . $record->company->name . ').pdf', [
+            'compress' => true,
+            'Attachment' => false,
+        ]);
     }
 
     public function download(Estimate $record)
     {
-        return $record;
+        $html = view('estimate.pdf', ['data' => $record])->render();
+        $tmp = sys_get_temp_dir();
+        $dompdf = new Dompdf([
+            'logOutputFile' => '',
+            'isRemoteEnabled' => true,
+            'fontDir' => $tmp,
+            'fontCache' => $tmp,
+            'tempDir' => $tmp,
+            'chroot' => $tmp,
+        ]);
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+        $dompdf->stream($record->reference . ' (' . $record->company->name . ').pdf', [
+            'compress' => true,
+            'Attachment' => true,
+        ]);
     }
 }
