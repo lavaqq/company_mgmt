098cc790e147d505e1606998fdcb8f633d8b2764
lavaqq <mlava@tuta.io>
2023-06-19 15:59:47 +0200

    fix: remove user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
             CompanySeeder::class,
             ContactSeeder::class,
             InvoiceSeeder::class,
