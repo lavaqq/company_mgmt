ab20a36aa90bd4c11f3334b349011b26f4588d19
lavaqq <mlava@tuta.io>
2023-06-19 09:33:35 +0200

    chore: remove policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,9 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array<class-string, class-string>
      */
-    protected $policies = [
-        User::class => UserPolicy::class,
-    ];
+    protected $policies = [];
 
     /**
      * Register any authentication / authorization services.
