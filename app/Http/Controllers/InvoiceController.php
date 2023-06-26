e48a8f239b201b7c231ed46a76407341f65536d8
lavaqq <mlava@tuta.io>
2023-06-26 15:50:51 +0200

    test: pdf

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -10,32 +10,32 @@ class InvoiceController extends Controller
 {
     public function show(Invoice $record)
     {
-        // $html = view('pdf.invoice', ['data' => $record])->render();
-        // $tmp = sys_get_temp_dir();
-        // $dompdf = new Dompdf([
-        //     'logOutputFile' => '',
-        //     'isRemoteEnabled' => true,
-        //     'fontDir' => $tmp,
-        //     'fontCache' => $tmp,
-        //     'tempDir' => $tmp,
-        //     'chroot' => $tmp,
-        // ]);
-        // $dompdf->loadHtml($html);
-        // $dompdf->render();
-        // $dompdf->stream($record->reference . ' (' . $record->company->name . ').pdf', [
-        //     'compress' => true,
-        //     'Attachment' => false,
-        // ]);
-
-        $pdf = Pdf::loadView('pdf.invoice', ['data' => $record]);
-        $pdf->setOption([
+        $html = view('pdf.invoice', ['data' => $record])->render();
+        $tmp = sys_get_temp_dir();
+        $dompdf = new Dompdf([
             'logOutputFile' => '',
             'isRemoteEnabled' => true,
-            'fontDir' => sys_get_temp_dir(),
-            'fontCache' => sys_get_temp_dir(),
-            'tempDir' => sys_get_temp_dir(),
-            'chroot' => sys_get_temp_dir(),
+            'fontDir' => $tmp,
+            'fontCache' => $tmp,
+            'tempDir' => $tmp,
+            'chroot' => $tmp,
         ]);
-        return $pdf->stream('invoice.pdf');
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+        $dompdf->stream($record->reference . ' (' . $record->company->name . ').pdf', [
+            'compress' => true,
+            'Attachment' => false,
+        ]);
+
+        // $pdf = Pdf::loadView('pdf.invoice', ['data' => $record]);
+        // $pdf->setOption([
+        //     'logOutputFile' => '',
+        //     'isRemoteEnabled' => true,
+        //     'fontDir' => sys_get_temp_dir(),
+        //     'fontCache' => sys_get_temp_dir(),
+        //     'tempDir' => sys_get_temp_dir(),
+        //     'chroot' => sys_get_temp_dir(),
+        // ]);
+        // return $pdf->stream('invoice.pdf');
     }
 }
