01d0c479b0cff219509ce36ee5134066ded0fdc1
lavaqq <mlava@tuta.io>
2023-06-26 20:09:34 +0200

    fix: pdf

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Invoice;
+use Illuminate\Http\Response;
 use Dompdf\Dompdf;
 
 
@@ -17,30 +18,72 @@ class InvoiceController extends Controller
         $dompdf->stream();
     }
 
+    //     public function show(Invoice $record)
+    //     {
+    //         // $html = view('pdf.invoice', ['data' => $record])->render();
+
+    //         // $tmp = sys_get_temp_dir();
+
+    //         // $dompdf = new Dompdf([
+    //         //     'logOutputFile' => '',
+    //         //     'isRemoteEnabled' => true,
+    //         //     'fontDir' => $tmp,
+    //         //     'fontCache' => $tmp,
+    //         //     'tempDir' => $tmp,
+    //         //     'chroot' => $tmp,
+    //         // ]);
+
+    //         // $dompdf->loadHtml($html);
+
+    //         // $dompdf->render();
+
+    //         // $dompdf->stream('test.pdf', [
+    //         //     'compress' => true,
+    //         //     'Attachment' => false,
+    //         // ]);
+    //         // exit();
+    //         $html = <<<HTML
+    // <!DOCTYPE html>
+    // <html>
+    // <head>
+    // <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    // <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+    // <style>
+    // .p {
+    //     font-family: 'Poppins';
+    // }
+    // </style>
+    // </head>
+    // <body>
+    //     <p class="p">
+    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    //     </p>
+    // </body>
+    // </html>
+    // HTML;
+
+    //         $tmp = sys_get_temp_dir();
+
+    //         $dompdf = new Dompdf([
+    //             'logOutputFile' => '',
+    //             'isRemoteEnabled' => true,
+    //             'fontDir' => $tmp,
+    //             'fontCache' => $tmp,
+    //             'tempDir' => $tmp,
+    //             'chroot' => $tmp,
+    //         ]);
+
+    //         $dompdf->loadHtml($html);
+    //         $dompdf->render();
+
+    //         $dompdf->stream('hello.pdf', [
+    //             'compress' => true,
+    //             'Attachment' => false,
+    //         ]);
+    //         exit();
+    //     }
     public function show(Invoice $record)
     {
-        // $html = view('pdf.invoice', ['data' => $record])->render();
-
-        // $tmp = sys_get_temp_dir();
-
-        // $dompdf = new Dompdf([
-        //     'logOutputFile' => '',
-        //     'isRemoteEnabled' => true,
-        //     'fontDir' => $tmp,
-        //     'fontCache' => $tmp,
-        //     'tempDir' => $tmp,
-        //     'chroot' => $tmp,
-        // ]);
-
-        // $dompdf->loadHtml($html);
-
-        // $dompdf->render();
-
-        // $dompdf->stream('test.pdf', [
-        //     'compress' => true,
-        //     'Attachment' => false,
-        // ]);
-        // exit();
         $html = <<<HTML
 <!DOCTYPE html>
 <html>
@@ -49,14 +92,14 @@ class InvoiceController extends Controller
 <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
 <style>
 .p {
-    font-family: 'Poppins';
+font-family: 'Poppins';
 }
 </style>
 </head>
 <body>
-    <p class="p">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    </p>
+<p class="p">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+</p>
 </body>
 </html>
 HTML;
@@ -75,10 +118,16 @@ HTML;
         $dompdf->loadHtml($html);
         $dompdf->render();
 
-        $dompdf->stream('hello.pdf', [
-            'compress' => true,
-            'Attachment' => false,
-        ]);
-        exit();
+        // Create the response object
+        $response = new Response($dompdf->output());
+
+        // Remove the "Content-Type" header
+        $response->headers->remove('Content-Type');
+
+        // Set the headers for PDF output
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', 'inline; filename="hello.pdf"');
+
+        return $response;
     }
 }
