337b3bf37cb098b5b171c1125c2552f00dedfebb
lavaqq <mlava@tuta.io>
2023-06-26 16:28:16 +0200

    pdf: test

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -7,12 +7,36 @@ use Dompdf\Dompdf;
 
 class InvoiceController extends Controller
 {
+    // public function show(Invoice $record)
+    // {
+    //     $html = view('pdf.invoice', ['data' => $record]);
+    //     $dompdf = new Dompdf();
+    //     $dompdf->loadHtml($html);
+    //     $dompdf->render();
+    //     $dompdf->stream();
+    // }
     public function show(Invoice $record)
     {
-        $html = view('pdf.invoice', ['data' => $record]);
-        $dompdf = new Dompdf();
+        $html = view('test', ['data' => $record])->render;
+
+        $tmp = sys_get_temp_dir();
+
+        $dompdf = new Dompdf([
+            'logOutputFile' => '',
+            'isRemoteEnabled' => true,
+            'fontDir' => $tmp,
+            'fontCache' => $tmp,
+            'tempDir' => $tmp,
+            'chroot' => $tmp,
+        ]);
+
         $dompdf->loadHtml($html);
+
         $dompdf->render();
-        $dompdf->stream();
+
+        $dompdf->stream('test.pdf', [
+            'compress' => true,
+            'Attachment' => false,
+        ]);
     }
 }
