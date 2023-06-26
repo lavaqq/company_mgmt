3fcdcc6747ca2523766b9b8dcc9a8e5d7e36e921
lavaqq <mlava@tuta.io>
2023-06-26 15:33:39 +0200

    test: pdf

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,15 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Invoice;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Dompdf\Dompdf;
 
 class InvoiceController extends Controller
 {
     public function show(Invoice $record)
     {
-        $html = view('pdf.invoice', ['data' => $record])->render();
-        $pdf = Pdf::setOption([
-            'defaultFont' => 'sans-serif'
-        ])->loadHTML($html);
-        return $pdf->stream($record->reference . ' (' . $record->company->name . ').pdf');
+        // $html = view('pdf.invoice', ['data' => $record])->render();
+        // $tmp = sys_get_temp_dir();
+        // $dompdf = new Dompdf([
+        //     'logOutputFile' => '',
+        //     'isRemoteEnabled' => true,
+        //     'fontDir' => $tmp,
+        //     'fontCache' => $tmp,
+        //     'tempDir' => $tmp,
+        //     'chroot' => $tmp,
+        // ]);
+        // $dompdf->loadHtml($html);
+        // $dompdf->render();
+        // $dompdf->stream($record->reference . ' (' . $record->company->name . ').pdf', [
+        //     'compress' => true,
+        //     'Attachment' => false,
+        // ]);
+
+        $pdf = Pdf::loadView('pdf.invoice', ['data' => $record]);
+        $pdf->setOption([
+            'logOutputFile' => '',
+            'isRemoteEnabled' => true,
+            'fontDir' => sys_get_temp_dir(),
+            'fontCache' => sys_get_temp_dir(),
+            'tempDir' => sys_get_temp_dir(),
+            'chroot' => sys_get_temp_dir(),
+        ]);
+        return $pdf->stream('invoice.pdf');
     }
 }
--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -22,6 +22,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <style>
         * {
             box-sizing: border-box;
@@ -182,6 +185,7 @@
     </style>
     <style>
         body {
+            font-family: 'Poppins';
             margin: 64px;
         }
 
@@ -202,7 +206,7 @@
             font-style: normal;
             font-weight: 500;
             font-size: 10px;
-            line-height: 140%;
+            line-height: 15px;
         }
 
         .uppercase {
