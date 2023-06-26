479e6802b487d36b104cf3b1a56739325587a432
lavaqq <mlava@tuta.io>
2023-06-26 20:05:25 +0200

    test: fake pdf

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -19,7 +19,47 @@ class InvoiceController extends Controller
 
     public function show(Invoice $record)
     {
-        $html = view('pdf.invoice', ['data' => $record])->render();
+        // $html = view('pdf.invoice', ['data' => $record])->render();
+
+        // $tmp = sys_get_temp_dir();
+
+        // $dompdf = new Dompdf([
+        //     'logOutputFile' => '',
+        //     'isRemoteEnabled' => true,
+        //     'fontDir' => $tmp,
+        //     'fontCache' => $tmp,
+        //     'tempDir' => $tmp,
+        //     'chroot' => $tmp,
+        // ]);
+
+        // $dompdf->loadHtml($html);
+
+        // $dompdf->render();
+
+        // $dompdf->stream('test.pdf', [
+        //     'compress' => true,
+        //     'Attachment' => false,
+        // ]);
+        // exit();
+        $html = <<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+<style>
+.p {
+    font-family: 'Poppins';
+}
+</style>
+</head>
+<body>
+    <p class="p">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+</body>
+</html>
+HTML;
 
         $tmp = sys_get_temp_dir();
 
@@ -33,13 +73,11 @@ class InvoiceController extends Controller
         ]);
 
         $dompdf->loadHtml($html);
-
         $dompdf->render();
 
-        $dompdf->stream('test.pdf', [
+        $dompdf->stream('hello.pdf', [
             'compress' => true,
             'Attachment' => false,
         ]);
-        exit();
     }
 }
