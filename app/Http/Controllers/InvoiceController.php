fd2b90ce1c37111ad9f69f4fc414269e8f03f342
lavaqq <mlava@tuta.io>
2023-06-26 16:11:38 +0200

    test: pdf

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Invoice;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Dompdf\Dompdf;
 
 class InvoiceController extends Controller
 {
     public function show(Invoice $record)
     {
-        $pdf = Pdf::loadView('pdf.invoice', ['data' => $record]);
-        $pdf->setOption([
-            'isRemoteEnabled' => true,
-            'fontDir' => sys_get_temp_dir(),
-            'fontCache' => sys_get_temp_dir(),
-            'tempDir' => sys_get_temp_dir(),
-        ]);
-        return $pdf->stream('invoice.pdf');
+
+
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml('hello world');
+
+        // (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A4', 'landscape');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser
+        $dompdf->stream();
     }
 }
