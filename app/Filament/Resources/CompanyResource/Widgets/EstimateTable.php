352b13ee1dcccc8c7c433bb956b63cfc9dff72a0
lavaqq <mlava@tuta.io>
2023-07-01 00:04:22 +0200

    feat(company): estimate widget table

---
+++ app/Filament/Resources/CompanyResource/Widgets/EstimateTable.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources\CompanyResource\Widgets;
+
+use App\Models\Contact;
+use App\Models\Estimate;
+use Closure;
+use Filament\Tables;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\TagsColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class EstimateTable extends BaseWidget
+{
+    protected static ?string $heading = 'Devis émis';
+
+    protected function getTableEmptyStateIcon(): ?string
+    {
+        return 'heroicon-o-document-text';
+    }
+
+    protected function getTableEmptyStateHeading(): ?string
+    {
+        return 'Aucun devis enregistré';
+    }
+
+    public ?Model $record = null;
+
+    protected function getTableQuery(): Builder
+    {
+        return Estimate::query()->where('company_id', $this->record->id);
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('reference')
+                ->label('Référence'),
+            TextColumn::make('company.name')
+                ->label('Entreprise')
+                ->limit(20),
+            TextColumn::make('issue_date')
+                ->label("Date d'émission")
+                ->dateTime('d/m/Y'),
+            TextColumn::make('total_excl_tax')
+                ->label('Total HT')
+                ->suffix(' €')
+                ->getStateUsing(fn (Model|null $record): float => $record->getTotalExcludingTax()),
+            TextColumn::make('total_incl_tax')
+                ->label('Total TTC')
+                ->suffix(' €')
+                ->getStateUsing(fn (Model|null $record): float => $record->getTotalIncludingTax()),
+            BadgeColumn::make('status')
+                ->label('Statut')
+                ->enum([
+                    'creation' => 'En création',
+                    'pending' => 'En attente',
+                    'signed' => 'Signé',
+                    'refused' => 'Refusé',
+                ])
+                ->colors([
+                    'secondary' => 'creation',
+                    'warning' => 'pending',
+                    'success' => 'signed',
+                    'danger' => 'refused',
+                ]),
+        ];
+    }
+
+    protected function getTableRecordUrlUsing(): ?Closure
+    {
+        return fn (Model $record): string => route('estimate.pdf', ['record' => $record]);
+    }
+}
