728889e1c5d798233ad90ed409db40ebb8ac6767
lavaqq <mlava@tuta.io>
2023-07-05 14:36:54 +0200

    feat(db): create leads table

---
+++ database/migrations/2023_07_05_123141_create_leads_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id');
+            $table->longText('title');
+            $table->enum('status', [
+                'new',
+                'waiting_for_contact',
+                'contacted',
+                'qualified',
+                'unqualified',
+            ]);
+            $table->date('start_date');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
