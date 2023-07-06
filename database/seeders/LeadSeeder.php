2c51bd0322f5c4a7785dd26d7a6a9d004cbf5a7e
lavaqq <mlava@tuta.io>
2023-07-07 00:10:07 +0200

    feat(seeder): lead

---
+++ database/seeders/LeadSeeder.php
@@ -0,0 +1,455 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Lead;
+use Illuminate\Database\Seeder;
+
+class LeadSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Club",
+            "status" => "qualified",
+            "start_date" => "2023-03-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Hello UGO",
+            "status" => "qualified",
+            "start_date" => "2023-02-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Archi 3D",
+            "status" => "qualified",
+            "start_date" => "2023-02-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Stand MyQM",
+            "status" => "qualified",
+            "start_date" => "2023-03-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "AACE",
+            "status" => "qualified",
+            "start_date" => "2023-02-22",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Uriel Piercer",
+            "status" => "qualified",
+            "start_date" => "2023-01-24",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Maquillage - Narcisse",
+            "status" => "qualified",
+            "start_date" => "2023-03-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Botalys",
+            "status" => "qualified",
+            "start_date" => "2022-11-09",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Rollup et flyeur MyQM",
+            "status" => "qualified",
+            "start_date" => "2023-03-02",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Réserve horaire Botalys",
+            "status" => "qualified",
+            "start_date" => "2023-02-22",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Site Prism video",
+            "status" => "qualified",
+            "start_date" => "2023-02-22",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Clicq consult",
+            "status" => "qualified",
+            "start_date" => "2023-01-13",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Archi 3D",
+            "status" => "qualified",
+            "start_date" => "2023-01-11",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Veiga",
+            "status" => "qualified",
+            "start_date" => "2022-09-02",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Hypocrédit",
+            "status" => "qualified",
+            "start_date" => "2022-08-24",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "REDSYSTEM",
+            "status" => "qualified",
+            "start_date" => "2022-09-02",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Formyfit App",
+            "status" => "qualified",
+            "start_date" => "2022-11-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "ANRO Agency",
+            "status" => "qualified",
+            "start_date" => "2023-01-05",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Site MyQM",
+            "status" => "qualified",
+            "start_date" => "2023-02-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Vecchia Napoli - site réservation",
+            "status" => "qualified",
+            "start_date" => "2023-04-02",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Mise à jour REDSYSTEM",
+            "status" => "qualified",
+            "start_date" => "2023-04-05",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Landing page Archit3D",
+            "status" => "qualified",
+            "start_date" => "2023-04-06",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Intégration landing page Archit3D",
+            "status" => "qualified",
+            "start_date" => "2023-04-06",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Franck Debue",
+            "status" => "qualified",
+            "start_date" => "2023-04-05",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Landing page - INL",
+            "status" => "qualified",
+            "start_date" => "2023-04-18",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Dealview - landing page",
+            "status" => "qualified",
+            "start_date" => "2023-05-05",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Benjamin POTIE - Intégration",
+            "status" => "qualified",
+            "start_date" => "2023-05-09",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Modification innovation-lab",
+            "status" => "qualified",
+            "start_date" => "2023-05-24",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Modifications zebauca",
+            "status" => "qualified",
+            "start_date" => "2023-05-26",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Suppression du malware du site d'Ingelec",
+            "status" => "qualified",
+            "start_date" => "2023-05-25",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Nouveau site Ingelec",
+            "status" => "qualified",
+            "start_date" => "2023-05-25",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Sam SirMeus",
+            "status" => "qualified",
+            "start_date" => "2023-06-06",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Formulaire Cabinet Monot",
+            "status" => "qualified",
+            "start_date" => "2023-06-12",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Adaptation Stand MyQM",
+            "status" => "qualified",
+            "start_date" => "2023-06-14",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Eude Djibero",
+            "status" => "qualified",
+            "start_date" => "2023-06-13",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Projet Nico Firebase",
+            "status" => "qualified",
+            "start_date" => "2023-06-16",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Franck Charlery-Adèle",
+            "status" => "qualified",
+            "start_date" => "2023-06-30",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Floryane Charlery-Adèle",
+            "status" => "qualified",
+            "start_date" => "2023-06-30",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Archit3D - landing page",
+            "status" => "unqualified",
+            "start_date" => "2023-03-20",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Tonneau Samuel",
+            "status" => "unqualified",
+            "start_date" => "2023-03-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Belrey",
+            "status" => "unqualified",
+            "start_date" => "2023-01-10",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Boutique OZE",
+            "status" => "unqualified",
+            "start_date" => "2023-04-04",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Objectif-events",
+            "status" => "unqualified",
+            "start_date" => "2023-03-06",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Projet Magic",
+            "status" => "unqualified",
+            "start_date" => "2023-02-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Allard Sport Equiment",
+            "status" => "unqualified",
+            "start_date" => "2023-04-21",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Les tourelles",
+            "status" => "unqualified",
+            "start_date" => "2023-02-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Chat GPT BOTALYS",
+            "status" => "unqualified",
+            "start_date" => "2023-05-25",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "E-shop Alin'attendu",
+            "status" => "unqualified",
+            "start_date" => "2023-05-30",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Hello UGO",
+            "status" => "unqualified",
+            "start_date" => "2023-02-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "App mini foot",
+            "status" => "unqualified",
+            "start_date" => "2023-06-19",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "IFID",
+            "status" => "contacted",
+            "start_date" => "2022-09-14",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Panelart",
+            "status" => "contacted",
+            "start_date" => "2023-03-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Atelie d'architecture Menier-Westrade",
+            "status" => "contacted",
+            "start_date" => "2023-03-01",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Dashan lite",
+            "status" => "waiting_for_contact",
+            "start_date" => "2023-06-15",
+            "origin" => "unknown",
+        ]);
+
+        Lead::create([
+            'company_id' => 1,
+            "title" => "Ghalan - copyrighting",
+            "status" => "new",
+            "start_date" => "2023-07-05",
+            "origin" => "unknown",
+        ]);
+    }
+}
