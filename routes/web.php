01186f5e3f11c3650922fbf5cf1637e8c79e4080
lavaqq <mlava@tuta.io>
2023-07-07 00:13:19 +0200

    feat(route): company relation test route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Models\Company;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
+
+Route::get('/test/company/{id}', function ($id) {
+    return Company::with('contacts', 'leads', 'deals', 'estimates', 'invoices', 'receivedInvoices')->find($id);
+});
