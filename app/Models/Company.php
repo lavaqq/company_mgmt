b2a291f974e9f392a012358ea980b29b926af830
lavaqq <mlava@tuta.io>
2023-07-06 11:11:47 +0200

    feat(db): estimate relation

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
@@ -38,5 +39,21 @@ class Company extends Model
     public function leads(): HasMany
     {
         return $this->HasMany(Lead::class);
+    }
+
+    /**
+     * Get all deals in relation with the record.
+     */
+    public function deals(): HasManyThrough
+    {
+        return $this->hasManyThrough(Lead::class, Deal::class);
+    }
+
+    /**
+     * Get all estimates in relation with the record.
+     */
+    public function estimates(): HasManyThrough
+    {
+        return $this->hasManyThrough(Deal::class, Estimate::class);
     }
 }
