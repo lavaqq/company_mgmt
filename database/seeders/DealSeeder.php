55d84e21c9e99bc016e504cf19e149a8a3c01aac
lavaqq <mlava@tuta.io>
2023-07-07 00:09:46 +0200

    feat(seeder): deal

---
+++ database/seeders/DealSeeder.php
@@ -0,0 +1,555 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Deal;
+use Illuminate\Database\Seeder;
+
+class DealSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Création visuel",
+            "status" => "won",
+            'deal_value' =>  130,
+            'actual_deal_value' =>  130,
+            "start_date" => "2022-11-07",
+            "signature_date" => "2022-11-20",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Création visuel",
+            "status" => "won",
+            'deal_value' =>  80,
+            'actual_deal_value' =>  80,
+            "start_date" => "2022-11-07",
+            "signature_date" => "2022-11-20",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Stand Formyfit",
+            "status" => "won",
+            'deal_value' =>  150,
+            'actual_deal_value' =>  150,
+            "start_date" => "2022-12-13",
+            "signature_date" => "2022-12-14",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Intégation",
+            "status" => "won",
+            'deal_value' =>  104.99,
+            'actual_deal_value' =>  104.99,
+            "start_date" => "2023-01-31",
+            "signature_date" => "2023-02-01",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Design UNAA",
+            "status" => "won",
+            'deal_value' =>  400,
+            'actual_deal_value' =>  300,
+            "start_date" => "2022-12-06",
+            "signature_date" => "2023-01-07",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Intégration ITpeak",
+            "status" => "won",
+            'deal_value' =>  3000,
+            'actual_deal_value' =>  3000,
+            "start_date" => "2022-11-09",
+            "signature_date" => "2022-11-16",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Site Botalys",
+            "status" => "won",
+            'deal_value' =>  7120,
+            'actual_deal_value' =>  7120,
+            "start_date" => "2022-11-07",
+            "signature_date" => "2022-11-28",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Inégration Veiga",
+            "status" => "won",
+            'deal_value' =>  1970,
+            'actual_deal_value' =>  1970,
+            "start_date" => "2022-12-16",
+            "signature_date" => "2023-01-10",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Hébergement Anro Magazine",
+            "status" => "won",
+            'deal_value' =>  60,
+            'actual_deal_value' =>  60,
+            "start_date" => "2022-11-07",
+            "signature_date" => "2022-11-21",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "App Formyfit",
+            "status" => "won",
+            'deal_value' =>  3650,
+            'actual_deal_value' =>  3650,
+            "start_date" => "2022-11-16",
+            "signature_date" => "2023-01-05",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Hypo Crédit",
+            "status" => "won",
+            'deal_value' =>  3500,
+            'actual_deal_value' =>  3500,
+            "start_date" => "2022-11-15",
+            "signature_date" => "2023-02-02",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Stand MyQM",
+            "status" => "won",
+            'deal_value' =>  212.5,
+            'actual_deal_value' =>  255,
+            "start_date" => "2023-03-06",
+            "signature_date" => "2023-03-08",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Hébergement Anro agency",
+            "status" => "won",
+            'deal_value' =>  120,
+            'actual_deal_value' =>  120,
+            "start_date" => "2023-02-15",
+            "signature_date" => "2023-02-16",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Club",
+            "status" => "won",
+            'deal_value' =>  250,
+            'actual_deal_value' =>  250,
+            "start_date" => "2023-03-09",
+            "signature_date" => "2023-03-13",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Rollup et flyer",
+            "status" => "won",
+            'deal_value' =>  148.75,
+            'actual_deal_value' =>  170,
+            "start_date" => "2023-03-15",
+            "signature_date" => "2023-03-15",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Réserve horaire - BOTALYS",
+            "status" => "won",
+            'deal_value' =>  1250,
+            'actual_deal_value' =>  1250,
+            "start_date" => "2023-03-22",
+            "signature_date" => "2023-03-22",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Mise à jour REDSYSTEM",
+            "status" => "won",
+            'deal_value' =>  100,
+            'actual_deal_value' =>  100,
+            "start_date" => "2023-04-04",
+            "signature_date" => "2023-04-05",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Landing page Archit3D",
+            "status" => "won",
+            'deal_value' =>  300,
+            'actual_deal_value' =>  300,
+            "start_date" => "2023-04-06",
+            "signature_date" => "2023-04-14",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Landing page - INL",
+            "status" => "won",
+            'deal_value' =>  350,
+            'actual_deal_value' =>  350,
+            "start_date" => "2023-04-18",
+            "signature_date" => "2023-04-24",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Site MyQM",
+            "status" => "won",
+            'deal_value' =>  5814,
+            'actual_deal_value' =>  5814,
+            "start_date" => "2023-04-20",
+            "signature_date" => "2023-05-02",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Dealview - landing page",
+            "status" => "won",
+            'deal_value' =>  400,
+            'actual_deal_value' =>  400,
+            "start_date" => "2023-05-05",
+            "signature_date" => "2023-05-08",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Modifications zebauca",
+            "status" => "won",
+            'deal_value' =>  200,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-05-29",
+            "signature_date" => "2023-05-30",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Suppression du malware du site d'Ingelec",
+            "status" => "won",
+            'deal_value' =>  180,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-05-30",
+            "signature_date" => "2023-05-30",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Franck Debue",
+            "status" => "won",
+            'deal_value' =>  120,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-04-12",
+            "signature_date" => "2023-06-02",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Formulaire Cabinet Monot",
+            "status" => "won",
+            'deal_value' =>  325,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-06-12",
+            "signature_date" => "2023-06-13",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Adaptation Stand MyQM",
+            "status" => "won",
+            'deal_value' =>  65,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-06-14",
+            "signature_date" => "2023-06-16",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Blog maquillage - Narcisse",
+            "status" => "negotiation",
+            'deal_value' =>  2000,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-03-23",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Vecchia Napoli - site réservation",
+            "status" => "negotiation",
+            'deal_value' =>  1070,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-04-03",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Nouveau site Ingelec",
+            "status" => "proposal",
+            'deal_value' =>  1920,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-05-30",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "TropiGrow",
+            "status" => "proposal",
+            'deal_value' =>  3200,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-06-07",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Eude Djibero",
+            "status" => "proposal",
+            'deal_value' =>  1470,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-06-16",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Projet Nico Firebase",
+            "status" => "discovery",
+            'deal_value' =>  3000,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-06-28",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Franck Charlery-Adèle",
+            "status" => "proposal",
+            'deal_value' =>  1570,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-07-03",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Floryane Charlery-Adèle",
+            "status" => "proposal",
+            'deal_value' =>  1570,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-07-03",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "App de jeux pour enfant",
+            "status" => "lost",
+            'deal_value' =>  4000,
+            'actual_deal_value' =>  0,
+            "start_date" => "2022-12-15",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Site prism video",
+            "status" => "lost",
+            'deal_value' =>  6070,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-02-21",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Clicq Consult",
+            "status" => "lost",
+            'deal_value' =>  3320,
+            'actual_deal_value' =>  0,
+            "start_date" => "2022-12-19",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "AACE",
+            "status" => "lost",
+            'deal_value' =>  7370,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-02-15",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Hebergement ZEBATUCA",
+            "status" => "lost",
+            'deal_value' =>  120,
+            'actual_deal_value' =>  0,
+            "start_date" => "2022-11-07",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Archi 3D",
+            "status" => "lost",
+            'deal_value' =>  3200,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-03-13",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Intégration landing page Archit3D",
+            "status" => "lost",
+            'deal_value' =>  360,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-04-07",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Benjamin POTIE - Intégration",
+            "status" => "lost",
+            'deal_value' =>  1870,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-05-09",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Modification innovation-lab",
+            "status" => "lost",
+            'deal_value' =>  180,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-05-24",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Hello UGO - App Client",
+            "status" => "lost",
+            'deal_value' =>  2000,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-03-09",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+
+        Deal::create([
+            'lead_id' => 1,
+            'company_id' => 1,
+            "title" => "Uriel Piercer",
+            "status" => "lost",
+            'deal_value' =>  3940,
+            'actual_deal_value' =>  0,
+            "start_date" => "2023-01-31",
+            "signature_date" => "null",
+            'note' => null
+        ]);
+    }
+}
