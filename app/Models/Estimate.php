24268d30f72b413bb3f8be5730aace139f9de7a3
lavaqq <mlava@tuta.io>
2023-07-07 09:54:42 +0200

    feat(model): estimate calculation

--- app/Models/Estimate.php
+++ app/Models/Estimate.php
@@ -45,4 +45,41 @@ class Estimate extends Model
     {
         return $this->hasMany(EstimateDiscount::class);
     }
+
+    public function getTotalExcludingTax(): float
+    {
+        $totalAmount = $this->items()->sum('amount');
+        $totalFixedDiscount = $this->discounts()->where('is_percentage', false)->sum('amount');
+        $totalPercentageDiscount = $this->discounts()->where('is_percentage', true)->sum('amount');
+        $totalAmount -= $totalFixedDiscount;
+        $totalAmount *= (1 - ($totalPercentageDiscount / 100));
+
+        return round($totalAmount, 2);
+    }
+
+    public function getTotalIncludingTax(): float
+    {
+        $totalAmount = $this->items()->sum('amount');
+        $totalFixedDiscount = $this->discounts()->where('is_percentage', false)->sum('amount');
+        $totalPercentageDiscount = $this->discounts()->where('is_percentage', true)->sum('amount');
+        $taxRate = $this->tax_rate;
+        $totalAmount -= $totalFixedDiscount;
+        $totalAmount *= (1 - $totalPercentageDiscount / 100);
+        $totalAmount *= (1 + $taxRate / 100);
+
+        return round($totalAmount, 2);
+    }
+
+    public function getTax(): float
+    {
+        $totalAmount = $this->items()->sum('amount');
+        $totalFixedDiscount = $this->discounts()->where('is_percentage', false)->sum('amount');
+        $totalPercentageDiscount = $this->discounts()->where('is_percentage', true)->sum('amount');
+        $taxRate = $this->tax_rate;
+        $totalAmount -= $totalFixedDiscount;
+        $totalAmount *= (1 - $totalPercentageDiscount / 100);
+        $totalTax = $totalAmount * $taxRate / 100;
+
+        return round($totalTax, 2);
+    }
 }
