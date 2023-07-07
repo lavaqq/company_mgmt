b219713b327c9eab8f7bbedf33768b114f9b92dc
lavaqq <mlava@tuta.io>
2023-07-07 09:02:44 +0200

    feat(command): add dev option to user:create command

--- app/Console/Commands/CreateUser.php
+++ app/Console/Commands/CreateUser.php
@@ -4,40 +4,42 @@ namespace App\Console\Commands;
 
 use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Hash;
 
 class CreateUser extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'user:create';
+    protected $signature = 'user:create {--dev : Create a developer user}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Manually creates an user.';
+    protected $description = 'Manually creates a user.';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        $first_name = $this->ask('Please enter an first name.');
-        $last_name = $this->ask('Please enter an last name.');
-        $email = $this->ask('Please enter an email.');
-        $password = $this->secret('Please enter a password.');
+        if ($this->option('dev') && !App::environment('local')) {
+            $this->error('The --dev option can only be used in the local environment.');
+            return;
+        }
+
+        if ($this->option('dev')) {
+            $firstName = 'dev';
+            $lastName = 'dev';
+            $email = 'dev@dev.io';
+            $password = 'dev';
+        } else {
+            $firstName = $this->ask('Please enter a first name.');
+            $lastName = $this->ask('Please enter a last name.');
+            $email = $this->ask('Please enter an email.');
+            $password = $this->secret('Please enter a password.');
+        }
+
         try {
             User::create([
-                'first_name' => $first_name,
-                'last_name' => $last_name,
+                'first_name' => $firstName,
+                'last_name' => $lastName,
                 'email' => $email,
                 'password' => Hash::make($password),
             ]);
+            $this->info('User created successfully!');
         } catch (\Exception $e) {
             $this->error($e->getMessage());
             return;
