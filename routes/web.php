d472290c292dcfd55cd5edc317930bb737e1167e
lavaqq <mlava@tuta.io>
2023-07-07 10:04:53 +0200

    feat(pdf): implement pdf routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Models\Company;
 use App\Models\Invoice;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstimateController;
+use App\Http\Controllers\InvoiceController;
+use Filament\Http\Middleware\Authenticate;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
+Route::middleware([Authenticate::class])->group(function () {
+    Route::get('invoices/{record}/pdf', [InvoiceController::class, 'show'])->name('invoice.pdf');
+    Route::get('estimates/{record}/pdf', [EstimateController::class, 'show'])->name('estimate.pdf');
+});
+
 Route::get('/test/company/{id}', function ($id) {
     return Company::with('contacts', 'leads', 'deals', 'estimates', 'invoices', 'receivedInvoices')->find($id);
 });
 
 Route::get('/test', function () {
-    return Invoice::first()->deal->lead->company;
+    return Company::first()->invoices;
 });
