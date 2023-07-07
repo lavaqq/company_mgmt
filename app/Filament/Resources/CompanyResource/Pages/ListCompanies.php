d394d1d7d1a237143bb7735940fdaad5130aeea9
lavaqq <mlava@tuta.io>
2023-07-07 10:17:34 +0200

    feat(filament): create list page of company

---
+++ app/Filament/Resources/CompanyResource/Pages/ListCompanies.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources\CompanyResource\Pages;
+
+use App\Filament\Resources\CompanyResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Closure;
+use Illuminate\Database\Eloquent\Model;
+
+class ListCompanies extends ListRecords
+{
+    protected static string $resource = CompanyResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('Créer une entreprise'),
+        ];
+    }
+
+    protected function getTitle(): string
+    {
+        return 'Liste des entreprises';
+    }
+
+    protected function getTableEmptyStateIcon(): ?string
+    {
+        return 'heroicon-o-briefcase';
+    }
+
+    protected function getTableEmptyStateHeading(): ?string
+    {
+        return 'Aucune entreprise enregistré';
+    }
+
+    protected function getTableRecordUrlUsing(): ?Closure
+    {
+        return fn (Model $record): string => route('filament.resources.companies.view', ['record' => $record]);
+    }
+}
