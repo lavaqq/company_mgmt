70b3b5913e3793b55354be1c94ea5b668b2910e5
lavaqq <mlava@tuta.io>
2023-07-11 14:55:12 +0200

    feat(filament): deal multiple deals table

---
+++ app/Filament/Resources/DealResource/Pages/MultiTableDeal.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Resources\DealResource\Pages;
+
+use App\Filament\Resources\DealResource;
+use Filament\Resources\Pages\Page;
+use Filament\Pages\Actions\Action;
+
+class MultiTableDeal extends Page
+{
+    protected static string $resource = DealResource::class;
+
+    protected static string $view = 'filament.resources.deal-resource.pages.multi-table-deal';
+
+    protected static ?string $breadcrumb = 'Listes';
+
+    protected static ?string $title = 'Gestion des deals';
+
+    protected function getActions(): array
+    {
+        return [
+            Action::make('deleted')
+                ->url(fn () => DealResource::getUrl('deleted'))
+                ->label('Voir les deals supprimés')
+                ->color('danger'),
+            Action::make('create')
+                ->url(fn () => DealResource::getUrl('create'))
+                ->label('Créer un deal'),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            DealResource\Widgets\ActiveDealTable::class,
+            DealResource\Widgets\WonDealTable::class,
+            DealResource\Widgets\LostDealTable::class,
+        ];
+    }
+
+    protected function getHeaderWidgetsColumns(): int|string|array
+    {
+        return 1;
+    }
+}
