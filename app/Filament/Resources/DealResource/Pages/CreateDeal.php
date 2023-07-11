6f19fe88c23fe5cb31870ce73796577db46eb0bd
lavaqq <mlava@tuta.io>
2023-07-11 14:54:42 +0200

    feat(filament): deal create page

---
+++ app/Filament/Resources/DealResource/Pages/CreateDeal.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\DealResource\Pages;
+
+use App\Filament\Resources\DealResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDeal extends CreateRecord
+{
+    protected static string $resource = DealResource::class;
+
+    protected function getTitle(): string
+    {
+        return 'Créer un deal';
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected static bool $canCreateAnother = false;
+}
