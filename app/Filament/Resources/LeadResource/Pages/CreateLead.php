8c8ff6082fc0759c0fa37172a3b11ae9aded666e
lavaqq <mlava@tuta.io>
2023-07-11 13:10:16 +0200

    feat(filament): leads create page

---
+++ app/Filament/Resources/LeadResource/Pages/CreateLead.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\LeadResource\Pages;
+
+use App\Filament\Resources\LeadResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateLead extends CreateRecord
+{
+    protected static string $resource = LeadResource::class;
+
+    protected function getTitle(): string
+    {
+        return 'Créer un lead';
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected static bool $canCreateAnother = false;
+}
