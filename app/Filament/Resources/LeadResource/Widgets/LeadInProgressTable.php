ea9dbc15ef63e65911745697ff907c5bb65b7a8e
lavaqq <mlava@tuta.io>
2023-07-11 13:10:54 +0200

    feat(filament): leads in progress table

---
+++ app/Filament/Resources/LeadResource/Widgets/LeadInProgressTable.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Filament\Resources\LeadResource\Widgets;
+
+use App\Filament\Resources\LeadResource;
+use App\Models\Lead;
+use Closure;
+use Filament\Tables;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Tables\Actions\Action;
+
+
+class LeadInProgressTable extends BaseWidget
+{
+    protected static ?string $heading = 'Leads en cours';
+
+    protected function getTableEmptyStateIcon(): ?string
+    {
+        return 'heroicon-o-briefcase';
+    }
+
+    protected function getTableEmptyStateHeading(): ?string
+    {
+        return 'Aucun lead en cours enregistré';
+    }
+
+    public ?Model $record = null;
+
+    protected function getTableQuery(): Builder
+    {
+        return Lead::query()->whereIn('status', ['new', 'waiting_for_contact', 'contacted']);
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('title')
+                ->label('Titre')
+                ->limit(30),
+            TextColumn::make('company.name')
+                ->label('Entreprise')
+                ->sortable()
+                ->limit(10),
+            BadgeColumn::make('status')
+                ->label('Statut')
+                ->enum([
+                    'new' => 'Nouveau',
+                    'waiting_for_contact' => 'En attente de contact',
+                    'contacted' => 'Contacté',
+                    'qualified' => 'Qualifié',
+                    'unqualified' => 'Non qualifié',
+                ])
+                ->colors([
+                    'secondary' => 'new',
+                    'secondary' => 'waiting_for_contact',
+                    'warning' => 'contacted',
+                    'success' => 'qualified',
+                    'danger' => 'unqualified',
+                ]),
+            TextColumn::make('start_date')
+                ->label('Date de début')
+                ->dateTime('d/m/Y'),
+            BadgeColumn::make('origin')
+                ->label('Origine')
+                ->enum([
+                    'unknown' => 'Inconnue',
+                    'inbound' => 'Entrante',
+                    'outbound' => 'Sortante',
+                ])
+                ->colors([
+                    'secondary' => 'unknown',
+                    'warning' => 'Sortante',
+                    'success' => 'Entrante',
+                ]),
+        ];
+    }
+
+    protected function getTableActions(): array
+    {
+        return [
+            Action::make('edit')
+                ->url(fn (Model|null $record): string => LeadResource::getUrl('edit', $record))
+                ->label('')
+                ->icon('heroicon-s-pencil'),
+            Tables\Actions\DeleteAction::make()
+                ->modalHeading(function (Model $record): string {
+                    return 'Supprimer : ' . $record->title;
+                })
+                ->label(''),
+        ];
+    }
+
+    protected function getTableBulkActions(): array
+
+    {
+        return [
+            Tables\Actions\DeleteBulkAction::make(),
+        ];
+    }
+
+    protected function getTableRecordUrlUsing(): ?Closure
+    {
+        return fn (Model|null $record): string => LeadResource::getUrl('edit', $record);
+    }
+}
