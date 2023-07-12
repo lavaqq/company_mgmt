edcffddc8241773410f01afb97745173cf3a1abe
lavaqq <mlava@tuta.io>
2023-07-12 14:02:25 +0200

    chore: delete all files

--- resources/views/filament/pages/user-account.blade.php
+++
@@ -1,8 +0,0 @@
-<x-filament::page>
-    <form wire:submit.prevent="submit">
-        {{ $this->form }}
-        <x-filament-support::button class="mt-6" type="submit">
-            Enregistrer
-        </x-filament-support::button>
-    </form>
-</x-filament::page>
