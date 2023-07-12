657b3ea1c7de807f9a9d8e077dec2fbf14fa5cfd
lavaqq <mlava@tuta.io>
2023-07-12 13:55:30 +0200

    feat(filament): add received invoice in the calculation (StatsOverview)

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Invoice;
+use App\Models\ReceivedInvoice;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 use Carbon\Carbon;
@@ -38,26 +39,44 @@ class StatsOverview extends BaseWidget
 
     public static function getCurrentYearTax(): string
     {
-        return Invoice::whereNotIn('status', ['cancelled', 'creation'])
+        $issuedTaxTotal = Invoice::whereNotIn('status', ['cancelled', 'creation'])
             ->whereYear('issue_date', Carbon::now()->year)
             ->get()
             ->sum(function ($invoice) {
                 return $invoice->getTax();
             });
+
+        $receivedTaxTotal = ReceivedInvoice::whereYear('issue_date', Carbon::now()->year)
+            ->get()
+            ->sum('tax');
+
+        $totalTax = $issuedTaxTotal - $receivedTaxTotal;
+
+        return $totalTax;
     }
+
 
     public static function getCurrentMonthTotalExclVat(): string
     {
         $startOfMonth = Carbon::now()->startOfMonth();
         $endOfMonth = Carbon::now()->endOfMonth();
 
-        return Invoice::whereNotIn('status', ['cancelled', 'creation'])
+        $issuedTotal = Invoice::whereNotIn('status', ['cancelled', 'creation'])
             ->whereBetween('issue_date', [$startOfMonth, $endOfMonth])
             ->get()
             ->sum(function ($invoice) {
                 return $invoice->getTotalExcludingTax();
             });
+
+        $receivedTotal = ReceivedInvoice::whereBetween('issue_date', [$startOfMonth, $endOfMonth])
+            ->get()
+            ->sum('total');
+
+        $totalExclVat = $issuedTotal - $receivedTotal;
+
+        return $totalExclVat;
     }
+
 
     protected function getCards(): array
     {
