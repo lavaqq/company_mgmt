87ca693621d0b1a804014d27e71ea60b45e3aba9
lavaqq <mlava@tuta.io>
2023-07-13 10:58:14 +0200

    fix: typo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Filament::registerViteTheme('resources/css/filament.css');
+        Filament::serving(function () {
+            Filament::registerViteTheme('resources/css/filament.css');
+        });
     }
 }
