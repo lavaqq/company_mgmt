b2e237541a54e5d2be270eee3b794194ff9ce3de
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-26 13:37:12 +0200

    feat(lead): model (fillable & relations)

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -43,5 +44,13 @@ class Company extends Model
     public function contacts(): BelongsToMany
     {
         return $this->belongsToMany(Contact::class);
+    }
+
+    /**
+     * Get the leads for the company.
+     */
+    public function leads(): HasMany
+    {
+        return $this->hasMany(Lead::class);
     }
 }
--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Lead extends Model
@@ -15,5 +16,19 @@ class Lead extends Model
      *
      * @var array<int, string>
      */
-    protected $fillable = [];
+    protected $fillable = [
+        'title',
+        'status',
+        'start_date',
+        'origin',
+        'note',
+    ];
+
+    /**
+     * Get the company that owns the lead.
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
