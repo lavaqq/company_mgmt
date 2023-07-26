844a20d2e2785e291138a09b3b4e721e51b5ffcb
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-26 11:11:01 +0200

    init laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,14 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            CompanySeeder::class,
-            ContactSeeder::class,
-            LeadSeeder::class,
-            DealSeeder::class,
-            InvoiceSeeder::class,
-            EstimateSeeder::class,
-            ReceivedInvoiceSeeder::class,
-        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,18 @@
 <?php
 
-use App\Http\Controllers\EstimateController;
-use App\Http\Controllers\InvoiceController;
-use App\Http\Controllers\ReceivedInvoiceController;
-use App\Models\Company;
-use Filament\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
 
-Route::middleware([Authenticate::class])->group(function () {
-    Route::get('invoices/{record}/pdf', [InvoiceController::class, 'show'])->name('invoice.pdf');
-    Route::get('estimates/{record}/pdf', [EstimateController::class, 'show'])->name('estimate.pdf');
-    Route::get('received-invoices/{record}/pdf', [ReceivedInvoiceController::class, 'show'])->name('received-invoice.pdf');
-});
-
-Route::get('/test/company/{id}', function ($id) {
-    return Company::with('contacts', 'leads', 'deals', 'estimates', 'invoices', 'receivedInvoices')->find($id);
-});
-
-Route::get('/test', function () {
-    return Company::first()->invoices;
+Route::get('/', function () {
+    return view('welcome');
 });
