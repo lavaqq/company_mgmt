6d64ed880fa4799afddf5d84649928b4129f4ffa
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-26 14:54:37 +0200

    feat(project): factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -17,7 +17,7 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(4),
         ];
     }
 }
