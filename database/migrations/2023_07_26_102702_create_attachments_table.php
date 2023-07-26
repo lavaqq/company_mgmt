4ba6b887d1bd45c39d139e769e34ef411b5799cd
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-26 13:23:27 +0200

    feat(attachment): migration

--- database/migrations/2023_07_26_102702_create_attachments_table.php
+++ database/migrations/2023_07_26_102702_create_attachments_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('attachments', function (Blueprint $table) {
             $table->id();
+            $table->string('file_path');
             $table->timestamps();
         });
     }
