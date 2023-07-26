e57ded789c9135be2c849f919c45e006d96f1782
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-26 14:12:42 +0200

    feat(invoice): migrations

--- database/migrations/2023_07_26_102533_create_invoices_table.php
+++ database/migrations/2023_07_26_102533_create_invoices_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('company_id');
+            $table->string('reference');
+            $table->string('vcs')->nullable();
+            $table->decimal('tax_rate', 10, 2);
+            $table->date('issue_date');
+            $table->date('due_date');
+            $table->enum('status', []); // TODO: need to fill enum
+            $table->foreignId('attachment_id')->nullable();
+            $table->boolean('in_accounting_software')->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2023_07_26_102540_create_invoice_discounts_table.php
+++ database/migrations/2023_07_26_102540_create_invoice_discounts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('invoice_discounts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('invoice_id');
+            $table->string('description');
+            $table->boolean('is_percentage');
+            $table->decimal('amount', 10, 2);
             $table->timestamps();
         });
     }
