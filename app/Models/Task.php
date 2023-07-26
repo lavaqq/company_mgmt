8646847408c096bde9de81c8ccfc85296aac4130
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-26 14:40:19 +0200

    feat(task): model (fillable and relations)

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
@@ -15,5 +16,23 @@ class Task extends Model
      *
      * @var array<int, string>
      */
-    protected $fillable = [];
+    protected $fillable = [
+        'title',
+        'note',
+        'status',
+        'duration_tmp_start',
+        'duration_tmp_stop',
+        'duration',
+        'estimated_duration',
+        'schedule_start',
+        'schedule_end',
+    ];
+
+    /**
+     * The users that belong to the task.
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
