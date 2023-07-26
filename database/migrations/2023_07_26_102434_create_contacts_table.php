9cab70bff1bdd48aa0dff906b5a58e51187d3a8f
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-26 13:26:28 +0200

    feat(contact): migration

--- database/migrations/2023_07_26_102434_create_contacts_table.php
+++ database/migrations/2023_07_26_102434_create_contacts_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('job_title')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
