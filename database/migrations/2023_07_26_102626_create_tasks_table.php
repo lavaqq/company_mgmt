e14e571d083d2f3fc8d994319c0f6c9ab7895788
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-26 14:39:57 +0200

    feat(task): migrations

--- database/migrations/2023_07_26_102626_create_tasks_table.php
+++ database/migrations/2023_07_26_102626_create_tasks_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->nullable();
+            $table->string('title');
+            $table->longText('note')->nullable();
+            $table->enum('status', []); // TODO: need to fill enum
+            $table->timestamp('duration_tmp_start')->nullable();
+            $table->timestamp('duration_tmp_stop')->nullable();
+            $table->integer('duration');
+            $table->integer('estimated_duration')->nullable();
+            $table->date('schedule_start')->nullable();
+            $table->date('schedule_end')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2023_07_26_102640_create_user_task_table.php
+++ database/migrations/2023_07_26_102640_create_user_task_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_task', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('task_id');
             $table->timestamps();
         });
     }
