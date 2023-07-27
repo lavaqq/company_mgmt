5dd1a609cd2d8e006c6b80f22d8695ff1f7f623d
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-27 15:06:20 +0200

    feat(seeding): seed fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,12 @@ namespace Database\Seeders;
 use App\Models\Company;
 use App\Models\CompanyAddress;
 use App\Models\CompanyInformation;
+use App\Models\CreditNote;
+use App\Models\Deal;
+use App\Models\Estimate;
+use App\Models\Invoice;
+use App\Models\Lead;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,10 +23,49 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Company::factory()
-            ->count(100)
+            ->count(20)
             ->hasAddress()
             ->hasInformation()
             ->hasContacts(5)
+            ->has(
+                Estimate::factory()
+                    ->hasItems(3)
+                    ->hasDiscounts(1)
+                    ->count(3)
+            )
+            ->hasReceivedInvoices(5)
+            ->create();
+
+        Invoice::factory()
+            ->hasItems(3)
+            ->hasDiscounts(1)
+            ->count(100)
+            ->state(new Sequence(
+                fn (Sequence $sequence) => ['company_id' => Company::all()->random()],
+            ))
+            ->has(
+                CreditNote::factory()
+                    ->state(new Sequence(
+                        fn (Sequence $sequence) => ['company_id' => Company::all()->random()],
+                    ))
+                    ->hasItems(3)
+                    ->hasDiscounts(1)
+                    ->count(1)
+            )
+            ->create();
+
+        Lead::factory()
+            ->count(100)
+            ->state(new Sequence(
+                fn (Sequence $sequence) => ['company_id' => Company::all()->random()],
+            ))
+            ->has(
+                Deal::factory()
+                    ->state(new Sequence(
+                        fn (Sequence $sequence) => ['company_id' => Company::all()->random()],
+                    ))
+                    ->count(2)
+            )
             ->create();
     }
 }
