eadde69b17cc6051bc9a149012f707cea95549f7
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-27 14:03:21 +0200

    feat: enum

--- database/migrations/2023_07_26_102626_create_tasks_table.php
+++ database/migrations/2023_07_26_102626_create_tasks_table.php
@@ -16,7 +16,12 @@ return new class extends Migration
             $table->foreignId('project_id')->nullable();
             $table->string('title');
             $table->longText('note')->nullable();
-            $table->enum('status', []); // TODO: need to fill enum
+            $table->enum('status', [
+                'done',
+                'stuck',
+                'working_on_it',
+                'in_progress',
+            ])->nullable();
             $table->timestamp('duration_tmp_start')->nullable();
             $table->boolean('is_running')->default(false);
             $table->integer('duration');
