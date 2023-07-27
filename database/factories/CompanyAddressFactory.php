5279191e486e90bb22160cec9ba7941db2935e1f
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-27 13:43:28 +0200

    fix: typo

--- database/factories/CompanyAddressFactory.php
+++ database/factories/CompanyAddressFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CompanyAdress>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CompanyAddress>
  */
-class CompanyAdressFactory extends Factory
+class CompanyAddressFactory extends Factory
 {
     /**
      * Define the model's default state.
