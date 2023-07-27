f7bfb2274b9ec62636db0837e89213bd3bf81de7
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-27 14:30:22 +0200

    feat(seeding): company

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\CompanyAddress;
+use App\Models\CompanyInformation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Company::factory()
+            ->count(100)
+            ->hasAddress()
+            ->hasInformation()
+            ->create();
     }
 }
