91e0bc52fc283db9bba350b5f0811d2d5d9dc639
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-07-28 11:20:08 +0200

    feat(creditNote): calculation

--- app/Models/CreditNote.php
+++ app/Models/CreditNote.php
@@ -47,4 +47,42 @@ class CreditNote extends Model
     {
         return $this->hasMany(CreditNoteDiscount::class);
     }
+
+    // TODO: adapt for credit notes
+    // public function getTotalExcludingTax(): float
+    // {
+    //     $totalAmount = $this->items()->sum('amount');
+    //     $totalFixedDiscount = $this->discounts()->where('is_percentage', false)->sum('amount');
+    //     $totalPercentageDiscount = $this->discounts()->where('is_percentage', true)->sum('amount');
+    //     $totalAmount -= $totalFixedDiscount;
+    //     $totalAmount *= (1 - ($totalPercentageDiscount / 100));
+
+    //     return round($totalAmount, 2);
+    // }
+
+    // public function getTotalIncludingTax(): float
+    // {
+    //     $totalAmount = $this->items()->sum('amount');
+    //     $totalFixedDiscount = $this->discounts()->where('is_percentage', false)->sum('amount');
+    //     $totalPercentageDiscount = $this->discounts()->where('is_percentage', true)->sum('amount');
+    //     $taxRate = $this->tax_rate;
+    //     $totalAmount -= $totalFixedDiscount;
+    //     $totalAmount *= (1 - $totalPercentageDiscount / 100);
+    //     $totalAmount *= (1 + $taxRate / 100);
+
+    //     return round($totalAmount, 2);
+    // }
+
+    // public function getTax(): float
+    // {
+    //     $totalAmount = $this->items()->sum('amount');
+    //     $totalFixedDiscount = $this->discounts()->where('is_percentage', false)->sum('amount');
+    //     $totalPercentageDiscount = $this->discounts()->where('is_percentage', true)->sum('amount');
+    //     $taxRate = $this->tax_rate;
+    //     $totalAmount -= $totalFixedDiscount;
+    //     $totalAmount *= (1 - $totalPercentageDiscount / 100);
+    //     $totalTax = $totalAmount * $taxRate / 100;
+
+    //     return round($totalTax, 2);
+    // }
 }
