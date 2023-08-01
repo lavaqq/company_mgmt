9d9d367874e8b2c0d438c207fdafcf6bccebef67
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-08-01 15:03:43 +0200

    chore: remove useless condition

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -62,10 +61,6 @@ class User extends Authenticatable implements FilamentUser, HasAvatar, HasName
 
     public function canAccessPanel(Panel $panel): bool
     {
-        if (App::environment('local')) {
-            return true;
-        }
-
         return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
     }
 
