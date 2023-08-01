30c0bb1d84ef48be679d9b52c2c7bd5d1f9aefb8
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-08-01 14:34:42 +0200

    chore: delete useless files

--- resources/views/filament/pages/home/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-<x-filament-panels::page class="fi-dashboard-page">
-
-    <h1 class="text-3xl font-extrabold">Hello, {{ auth()->user()->first_name }}</h1>
-
-    <a href="/crm">crm</a>
-    <a href="/pm">pm</a>
-
-</x-filament-panels::page>
