1a08b8987c5eaf5dc663884cf4669775c3f83ec7
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-08-02 12:36:53 +0200

    feat(filament): create pm panel

---
+++ app/Filament/Hub/Widgets/PMNavigation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Hub\Widgets;
+
+use Filament\Widgets\Widget;
+
+class PMNavigation extends Widget
+{
+    protected static string $view = 'filament.hub.widgets.pm-navigation';
+}
