68658927e9d56a6a6898d9ccd8ba8e7febd065cb
lavaqq <81957562+lavaqq@users.noreply.github.com>
2023-08-08 13:02:00 +0200

    feat(credit note): status enum class

---
+++ app/Enums/CreditNoteStatus.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Contracts\HasLabel;
+
+enum CreditNoteStatus: string implements HasLabel
+{
+    case CREATION = 'creation';
+    case SENDED = 'sended';
+
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::CREATION => 'En création',
+            self::SENDED => 'Envoyée',
+        };
+    }
+}
